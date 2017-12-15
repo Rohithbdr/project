8dc3faad4025090eb01566946ed725cbbfc715c8
Naveen Bora <bnaveen.15.cse@anits.edu.in>
2017-12-15 13:32:00 +0530

    Add files via upload

---
+++ c.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(void)
+{
+  int i;
+  for(i=1; i <= 5; i++)
+  {
+     printf("%d ", i*i);
+  }
+  for(i=1; i <= 5; i++)
+  {
+     printf("%d ", i*i);
+  }
+  return 0;
+}
